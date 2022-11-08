2fcb310a6ccb2a7a9e16d4f3e1db1e769d039d3d
박준범 <@DESKTOP-2R821IK>
2022-11-08 10:56:26 +0900

    test3

--- testMnuproejct/src/testMnuproejct/Main.java
+++ testMnuproejct/src/testMnuproejct/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("203620 신도훈");
-		
+		System.out.println("193909 박준범");
 	}
 
 }
