b47c9ec89ab64cf353b65340e45de7e63b6281c2
a0103 <a0103@BOOK-9TDRM4AB7G>
2022-11-08 12:18:04 +0900

    testing

--- testMnuproejct/src/testMnuproejct/Main.java
+++ testMnuproejct/src/testMnuproejct/Main.java
@@ -6,9 +6,15 @@ public class Main {
 		// TODO Auto-generated method stub
 <<<<<<< HEAD
 		System.out.println("203632 - tae min");
+		
+=======
 		System.out.println("203620 신도훈");
 		System.out.println("193909 박준범");
 		System.out.println("193620 손정현");
+		System.out.println("193620 손정현 테스트중입니다.");
+		System.out.println("오후 12:17");
+		
+		
 >>>>>>> refs/remotes/origin/main
 	}
 
