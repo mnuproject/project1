85de45dac8a07b02a75837b5c4ec93f81d1814d9
CE312 <CE312@DESKTOP-FRL1S3N>
2022-11-08 10:59:00 +0900

    203632

--- testMnuproejct/src/testMnuproejct/Main.java
+++ testMnuproejct/src/testMnuproejct/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("203620 ½ÅµµÈÆ");
+		System.out.println("203632 - tae min");
 		
 	}
 
