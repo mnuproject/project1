3c0c5996f244d12f8580f37c3bd882658deeb827
user <user@DESKTOP-PR41A4C>
2022-11-11 11:33:21 +0900

    한글 깨짐?

--- testMnuproejct/src/testMnuproejct/Main.java
+++ testMnuproejct/src/testMnuproejct/Main.java
@@ -7,8 +7,8 @@ public class Main {
 		System.out.println("203632 - tae min");
 		System.out.println("193620 손정현 2022-11-11 다시 테스트중 왤캐 안되냐 짱난다.");
 		System.out.println("한글아 깨지지마");
-
-		
+		System.out.println("203620 신동훈 2022.11.11");
+		System.out.println("한글 hangul");
 	}
 
 }
