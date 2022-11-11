d98953bb56421d2218a2f970989bc0b7e03e2762
kimgo <kimgo@192.168.25.8>
2022-11-11 19:27:15 +0900

    173311 김준형 22.11.11 19시 24분 업로드

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("팀 D조 -과제용으로 한 번 다시 만든 프로젝트");
 		System.out.println("203620 신동훈");
+		
+		System.out.println("221111 19시 24분 깃허브 연동");
+		System.out.println("173311 김준형");
+		
 	}
 
 }
