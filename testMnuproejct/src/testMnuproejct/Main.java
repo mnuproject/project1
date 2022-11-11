5c71a1e742d2d09ee0666b8b9dc4a3937bd83801
a0103 <a0103@BOOK-9TDRM4AB7G>
2022-11-11 11:12:33 +0900

    한글아 깨지지마

--- testMnuproejct/src/testMnuproejct/Main.java
+++ testMnuproejct/src/testMnuproejct/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("203632 - tae min");
 		System.out.println("193620 손정현 2022-11-11 다시 테스트중 왤캐 안되냐 짱난다.");
-		
+		System.out.println("한글아 깨지지마");
 
 		
 	}
