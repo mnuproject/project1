6f410e2d4e170853cdd86952d89c94b383f191c1
mnuproject <117696034+mnuproject@users.noreply.github.com>
2022-12-11 11:28:19 +0900

    Update Ui2.java

--- catchmindGamePrj/src/UI/Ui2.java
+++ catchmindGamePrj/src/UI/Ui2.java
@@ -50,7 +50,7 @@ public class Ui2 extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				MainFrame.clnt.sendId(tfIdInput.getText());
-				uiTool.setUI(Ui2.this, Ui6.getUi6());
+				uiTool.setUI(Ui2.this, Ui3.getUi3());
 				//uiTool.setUI(Ui2.this, new Ui3());
 			}
 		});
