762b80adba9bab07e62afc3e973c887e019d76ef
kailon17 <107461884+kailon17@users.noreply.github.com>
2022-12-12 02:30:21 +0900

    Update Ui7.java

--- catchmindGamePrj/src/UI/Ui7.java
+++ catchmindGamePrj/src/UI/Ui7.java
@@ -7,42 +7,42 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
+import gameSound.GameSound;
+
 public class Ui7 extends JPanel{
 	public static Ui7 ui7;
-	
+	private GameSound sound;
 	private UiTool uiTool;
-	private JLabel plId1;
-	private JLabel plId2;
 	private JButton plBtn1;
 	private JButton plBtn2;
 	private JLabel plImg1;
 	
 	private String[] columnType = {"순위", "이름", "점수"};
 	private Object[][] data = {
-		       {"1", "A", "100"},
-		       {"2", "B", "85"},
-		       {"3", "C", "77"},
-		       {"4", "D", "50"}
+		       {"1", "A", "**"},
+		       {"2", "B", "**"},
+		       {"3", "C", "**"},
+		       {"4", "D", "**"}
 	};
 	
-	
-	//server로 부터 받은 데이터
-	//아이디 , 점수
 	public static Object[][] data1 = {
-				{"", "", ""},
-				{"", "", ""},
-				{"", "", ""},
-				{"", "", ""}
-	};
+			{"", "", ""},
+			{"", "", ""},
+			{"", "", ""},
+			{"", "", ""}
+};
+	public static String readidlist = "aa&bb&cc&null";
+	public static String readscorelist = "";
+	private String idlist[];
 	
-	
-	private Ui7() {
+	public Ui7() {
+		//sound = new GameSound("bgm/bg1.wav");
 		uiTool = new UiTool();
 		setLayout(null);
 		uI7_DesignLayout();
 		uI71_listener();
 		uI72_listener();
-	}
+	}	
 	
 	public static Ui7 getUi7() {
 		if (ui7 == null) {
@@ -51,64 +51,52 @@ public class Ui7 extends JPanel{
 		return ui7;
 	}
 	
-
-	
 	private void uI7_DesignLayout() {
+		//for (int i=0;i<4;i++) {
+			//idlist[i] = readidlist.split("&")[i];
+			//System.out.println(idlist[i]);
+		//}
+		
+		plImg1 = new JLabel();
+		plImg1.setIcon(uiTool.getImg("img/cong.png", 1100, 750));
+		plImg1.setBounds(0,0,1100,750);
+		
 		JPanel titlePanel = new JPanel();
 		titlePanel.setLayout(null);
-		titlePanel.setBackground(new Color(255, 255, 0));
+		titlePanel.setBackground(new Color(13, 152, 186));
 		titlePanel.setBounds(345, 70, 400, 100);
 		JLabel playTitle = new JLabel("최종 결과");
 		playTitle.setFont(uiTool.ftMedium());
 		playTitle.setBounds(145, 30, 200, 30);
 		titlePanel.add(playTitle);
 		
-		plId1 = new JLabel("UI7 라벨생성 예시1");
-		plId1.setBackground(new Color(255, 215, 0));
-		plId1.setBounds(00, 50, 400, 50);
-		
-		plId2 = new JLabel("UI7 라벨생성 예시2");
-		plId2.setBackground(new Color(255, 215, 0));
-		plId2.setBounds(1000, 100, 400, 50);
-		
-		JTable table = new JTable(data1, columnType);
-		table.setRowHeight(50);
-		table.setIntercellSpacing(new Dimension(5,2));
+		DefaultTableModel dtm = new DefaultTableModel(data, columnType);
+		ColorTable jtable = new ColorTable(dtm);
+		jtable.setRowHeight(50);
+		jtable.setIntercellSpacing(new Dimension(5,2));
 		DefaultTableCellRenderer celAlignCenter = new DefaultTableCellRenderer();
 		celAlignCenter.setHorizontalAlignment(JLabel.CENTER);
-		table.getColumn("순위").setCellRenderer(celAlignCenter);
-		table.getColumn("이름").setCellRenderer(celAlignCenter);
-		table.getColumn("점수").setCellRenderer(celAlignCenter);
-		table.setBackground(new Color(255, 0, 255));
-		table.setBackground(new Color(0, 255, 255));
+		jtable.setFont(new Font("굴림", Font.BOLD, 20));
+		jtable.getColumn("순위").setCellRenderer(celAlignCenter);
+		jtable.getColumn("이름").setCellRenderer(celAlignCenter);
+		jtable.getColumn("점수").setCellRenderer(celAlignCenter);
 		
-		JScrollPane jc = new JScrollPane(table);
+		JScrollPane jc = new JScrollPane(jtable);
 		jc.setBounds(345, 170, 400, 223);
 		
 		plBtn1 = new JButton("방선택");
 		plBtn1.setBackground(new Color(255, 255, 255));
-		plBtn1.setBounds(345, 500, 150, 50);
+		plBtn1.setBounds(345, 420, 150, 50);
 		
 		plBtn2 = new JButton("나가기");
 		plBtn2.setBackground(new Color(255, 255, 255));
-		plBtn2.setBounds(595, 500, 150, 50);
-		
-		plImg1 = new JLabel();
-		plImg1.setIcon(uiTool.getImg("img/drawBlackPen.png", 50, 100));
-		plImg1.setBounds(150, 100, 100, 100);
-		
-		JPanel IdProfile = new JPanel();
-		IdProfile.setLayout(null);
-		IdProfile.setBackground(new Color(239, 228, 176));
-		IdProfile.setBounds(0, 0, 1090, 715);
+		plBtn2.setBounds(595, 420, 150, 50);
 		
 		add(titlePanel);
-		//add(IdProfile);
-		//add(plId2);
 		add(jc);
 		add(plBtn1);
 		add(plBtn2);
-		//add(plImg1);
+		add(plImg1);
 	}
 	
 	class ColorTable extends JTable {
@@ -121,7 +109,18 @@ public class Ui7 extends JPanel{
 		public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
 			// TODO Auto-generated method stub
 			JComponent component = (JComponent) super.prepareRenderer(renderer, row, column);
-			component.setBackground(Color.YELLOW);
+			if (row==0) {
+				component.setBackground(new Color(255, 215, 0));
+			}
+			else if (row==1) {
+				component.setBackground(new Color(192, 192, 192));
+			}
+			else if (row==2) {
+				component.setBackground(new Color(196, 156, 49));
+			}
+			else if (row==3) {
+				component.setBackground(new Color(255, 255, 255));
+			}
 			return component;
 		}
 	}
@@ -139,7 +138,6 @@ public class Ui7 extends JPanel{
 		plBtn2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				MainFrame.clnt.sendExit();
 				System.exit(0);
 			}
 		});
