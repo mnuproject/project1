53ab7cd84056d5047f1a3045b62088b50d2c30d1
mnuproject <117696034+mnuproject@users.noreply.github.com>
2022-12-15 19:51:01 +0900

    Update Word_Ref.java

--- catchmindGamePrj/src/res/Word_Ref.java
+++ catchmindGamePrj/src/res/Word_Ref.java
@@ -12,6 +12,11 @@ public class Word_Ref {
 			"https://github.com/ehszl409/project_catchmindGame" + "\n\n" +
 			"[이미지]" + "\n" +
 			"https://github.com/ehszl409/project_catchmindGame" + "\n" +
+			"https://m.gettyimagesbank.com/search/%EC%9D%B4%EB%AF%B8%EC%A7%80/%EB%AD%89%EA%B2%8C%EA%B5%AC%EB%A6%84?lv=&st=union&page=1&q=%EB%AD%89%EA%B2%8C%EA%B5%AC%EB%A6%84" + "\n" +
+			"https://kr.123rf.com/photo_73544915_%EB%8F%99%EB%AC%BC-%EC%95%84%EC%9D%B4%EC%BD%98-%EB%AA%A8%EC%9D%8C-%ED%94%8C%EB%9E%AB-%EB%8F%99%EB%AC%BC-%EC%84%A4%EC%A0%95%ED%95%A9%EB%8B%88%EB%8B%A4-%EB%B2%A1%ED%84%B0-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-%EB%A0%88%EC%9D%B4-%EC%85%98.html" + "\n" +
+			"https://kr.freepik.com/premium-vector/cute-food-illustration-character_24406555.htm" + "\n" +
+			"https://kr.freepik.com/premium-vector/set-of-fresh-fruits-icons-illustration_22406571.htm" + "\n" +
+			"https://www.gamemeca.com/view.php?gid=1672900" + "\n" +
 			"https://kr.freepik.com/free-vector/celebration-confetti-with-podium-platform-on-white-background_16738212.htm#query=%EC%B6%95%ED%95%98&position=2&from_view=search&track=sph" + "\n\n" +
 			"[BGM]" + "\n" +
 			"https://m.blog.naver.com/edcc3080/221783348866" + "\n" +
